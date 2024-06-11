2f2e78517caa890bb0ed9045b471e4bad357822b
Debasis08 <patrodebasis8@gmail.com>
2024-06-11 13:13:29 +0530

    Library components Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -11,6 +13,8 @@ export default function Home() {
     <FeaturedCourses />
     <WhyChooseUs />
     <MusicSchoolTestimonials />
+    <UpcomingWebinars />
+    <Instructors />
     </main>
   );
 }
